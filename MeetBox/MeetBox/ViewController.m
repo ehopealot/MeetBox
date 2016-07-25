5cccedb396bc69ffed6e1981bfbc9dd53646a8c6
Erik Hope <ehope@dropbox.com>
2016-07-25 13:21:52 -0700

    setup buttons

--- MeetBox/MeetBox/ViewController.m
+++ MeetBox/MeetBox/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIButton *connectDropboxButton;
+@property (nonatomic, strong) UIButton *connectGCalButton;
+
 @end
 
 @implementation ViewController
@@ -17,6 +20,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    _connectDropboxButton = [[UIButton alloc] initWithFrame:CGRectZero];
+    [_connectDropboxButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+
+    _connectGCalButton = [[UIButton alloc] initWithFrame:CGRectZero];
+    [_connectGCalButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+
+    [self.view addSubview:_connectDropboxButton];
+    [self.view addSubview:_connectGCalButton];
+
+    [_connectDropboxButton setTitle:@"Connect Dropbox" forState:UIControlStateNormal];
+    [_connectGCalButton setTitle:@"Connect Google Calendar" forState:UIControlStateNormal];
+
+    [_connectDropboxButton addTarget:self action:@selector(connectDropbox:) forControlEvents:UIControlEventTouchUpInside];
+    [_connectGCalButton addTarget:self action:@selector(connectGoogle:) forControlEvents:UIControlEventTouchUpInside];
+
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[_connectDropboxButton(40)]-20-[_connectGCalButton(40)]"
+                                                                      options:NSLayoutFormatAlignAllCenterX metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(_connectDropboxButton, _connectGCalButton)]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[_connectDropboxButton(300)]"
+                                                                      options:0 metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(_connectDropboxButton)]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[_connectGCalButton(300)]"
+                                                                      options:0 metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(_connectGCalButton)]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_connectDropboxButton attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1 constant:-10]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_connectDropboxButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+
+
+    [_connectDropboxButton setBackgroundColor:[UIColor redColor]];
+    [_connectGCalButton setBackgroundColor:[UIColor redColor]];
+}
+
+- (void)connectDropbox:(id)sender {
+
+}
+
+- (void)connectGoogle:(id)sender {
+
 }
 
 - (void)didReceiveMemoryWarning {
