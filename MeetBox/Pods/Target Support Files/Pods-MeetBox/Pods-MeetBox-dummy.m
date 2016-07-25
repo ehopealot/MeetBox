51ea0555b0ac194d6b58a6a9f6ec30c6b4b45841
Erik Hope <ehope@dropbox.com>
2016-07-25 13:09:29 -0700

    cocoapods

---
+++ MeetBox/Pods/Target Support Files/Pods-MeetBox/Pods-MeetBox-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeetBox : NSObject
+@end
+@implementation PodsDummy_Pods_MeetBox
+@end
---
+++ MeetBox/Pods/Target Support Files/Pods-MeetBox/Pods-MeetBox-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MeetBoxVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MeetBoxVersionString[];
+
